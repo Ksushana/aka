feee23418a1d49d6fde2051ca47d2cd0ca303a1c
Ksushana <31410971+Ksushana@users.noreply.github.com>
2021-10-11 19:40:24 +0200

    fix

--- src/js/pages/all/index.js
+++ src/js/pages/all/index.js
@@ -138,7 +138,7 @@ if(randomize) {
     imageButton.addEventListener('click', onGenerateBtnClick, false);
 }
 
-const imageElem = document.querySelector('.image-share-block');
+const imageElem = document.querySelector('.image-share-block img');
 if (imageElem) {
     imageElem.src="https://img.bruzu.com/?bi=https://source.unsplash.com/27HiryxnHJk/500x500&bi.o=undefined&h=500&w=500&a.tp=textbox&a.ox=center&a.oy=center&a.x=250&a.y=250&a.w=503&a.h=122&a.t=" + window.location.search.replace('?', '') + "&a.ta=center&a.fs=60&a.lh=0.8&a.fw=700&a.ff=Space Grotesk&a.maxHeight=500";
 }
