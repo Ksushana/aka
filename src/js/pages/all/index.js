dd2b227882e3d9de216b0a39882ffcb5f6c57f55
Ksushana <31410971+Ksushana@users.noreply.github.com>
2021-10-15 10:28:47 +0200

    js build

--- src/js/pages/all/index.js
+++ src/js/pages/all/index.js
@@ -167,7 +167,7 @@ if(randomize) {
 
 const imageElem = document.querySelector('.image-share-block img');
 if (imageElem) {
-    imageElem.src="https://img.bruzu.com/?bi=https://ucarecdn.com/8aeee6e6-1eea-4575-a6fb-7f3a53055868/&bi.o=undefined&h=1080&w=1080&c.tp=textbox&c.ox=center&c.oy=center&c.x=540&c.y=517&c.w=860&c.h=293&c.t=" + window.location.search.replace('?', '') + "&c.ta=center&c.fs=120&c.lh=1&c.fw=700&c.ff=Roboto Condensed&c.maxHeight=488";
+    imageElem.src="https://img.bruzu.com/?bi=https://ucarecdn.com/8aeee6e6-1eea-4575-a6fb-7f3a53055868/&bi.o=undefined&h=1080&w=1080&a.tp=textbox&a.ox=center&a.oy=center&a.x=540&a.y=487&a.w=860&a.h=271&a.t=" + window.location.search.replace('?', '') + "&a.ta=center&a.fs=120&a.lh=1&a.fw=700&a.ff=Roboto Condensed&a.maxHeight=560";
     var metaTags=document.getElementsByTagName("meta");
 
     var fbAppIdContent = "";
